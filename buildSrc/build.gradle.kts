57bede401404c8bfed586e0011853f078591a9d1
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2023-10-09 20:37:44 -0600

    task: fix squareup javapoet library

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,10 @@ repositories {
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
   implementation("com.android.tools.build:gradle:8.0.2")
+
+  // https://github.com/google/dagger/issues/3068#issuecomment-999118496
+  // Required to fix a missing dependency error
+  implementation("com.squareup:javapoet:1.13.0")
 }
 
 val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
