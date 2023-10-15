ede89f5c24a38734b11695b060f7065eb45d1274
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2023-10-14 22:31:06 -0600

    task: Connect network and implement search Screen to connet to api

--- app/src/main/java/com/gilbertohdz/calorytracker/MainActivity.kt
+++ app/src/main/java/com/gilbertohdz/calorytracker/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.gilbertohdz.calorytracker.navigation.navigate
 import com.gilbertohdz.calorytracker.ui.theme.CaloryTrackerTheme
 import com.gilbertohdz.core.navigation.Route
@@ -22,9 +25,11 @@ import com.gilbertohdz.onboarding_presentation.height.HeightScreen
 import com.gilbertohdz.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.gilbertohdz.onboarding_presentation.weight.WeightScreen
 import com.gilbertohdz.onboarding_presentation.welcome.WelcomeScreen
+import com.gilbertohdz.tracker_presentation.search.SearchScreen
 import com.gilbertohdz.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalComposeUiApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
@@ -81,8 +86,38 @@ class MainActivity : ComponentActivity() {
                         composable(Route.TRACKER_OVERVIEW) {
                             TrackerOverviewScreen(onNavigate = navController::navigate)
                         }
-                        composable(Route.SEARCH) {
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
 
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                }
+                            )
                         }
                     }
                 }
