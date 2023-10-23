5bea43bb032d0e6551bf62e11b3609675e6bdaee
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2023-10-23 00:16:15 -0600

    task: Implement Traker End to End Instrumented testing
    * Fixing linkeding.dexmarker library importor by mockkVersion
    
    * Fixing SetContent compose rule by activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         versionCode = ProjectConfig.versionCode
         versionName = ProjectConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.gilbertohdz.calorytracker.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
